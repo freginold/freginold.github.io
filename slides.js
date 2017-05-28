8ea7ba63b432ab53f34bd5ca3db09b98822b1abe
freginold <freginold@users.noreply.github.com>
2017-05-28 09:03:43 -0400

    add showHTML Firefox link

---
+++ slides.js
@@ -0,0 +1,88 @@
+
+// slide properties declaration
+
+// browser extension slides
+var extSlides = [
+    {
+        img: "codeTabber128.png",
+        title: "codeTabber",
+        descr: "Switch between code panes on CodePen, JSFiddle, and other online code editors, right from the keyboard.",
+        chrome: "https://chrome.google.com/webstore/detail/codetabber/hhlhpibdcfdggipnplpdaboblfblppon",
+        firefox: "https://addons.mozilla.org/en-US/firefox/addon/codetabber/",
+        opera: "https://addons.opera.com/en/extensions/details/codetabber/"
+    },
+    {
+        img: "deArchive128.png",
+        title: "deArchive",
+        descr: "Remove the Archive button from Yahoo Mail, and put the Delete button back where it used to be.",
+        chrome: "https://chrome.google.com/webstore/detail/dearchive/cgniccpehldobhkdaknmphkoomkpbpac"
+    },
+    {
+        img: "followhound128.png",
+        title: "Follow Hound",
+        descr: "Quickly and easily see which Twitter users follow you back.",
+        chrome: "https://chrome.google.com/webstore/detail/follow-hound/fgogafjbjejkpclehlmkinbflgkmdmpf",
+        firefox: "https://addons.mozilla.org/en-US/firefox/addon/follow-hound/"
+    },
+    {
+        img: "likey128.png",
+        title: "Likey",
+        descr: "Quickly see which Facebook posts you've liked, which ones you haven't, and which ones are ads.",
+        chrome: "https://chrome.google.com/webstore/detail/likey/ehhdabnfnlpfbnpfefdkaokegojlpmin"
+    },
+    {
+        img: "loginFocus128.png",
+        title: "loginFocus",
+        descr: "Add autofocus to websites' login boxes.",
+        chrome: "https://chrome.google.com/webstore/detail/loginfocus/egkaagndoijoeojpecjepkepacaeblgf"
+    },
+    {
+        img: "mentionIt128.png",
+        title: "Mention It",
+        descr: "Replace Twitter's Moments tab with a Mentions tab.",
+        chrome: "https://chrome.google.com/webstore/detail/mention-it/gocmaoekbehkaffkagkfomjohipbggne",
+        opera: "https://addons.opera.com/en/extensions/details/mention-it/"
+    },
+    {
+        img: "showHTML128.png",
+        title: "showHTML",
+        descr: "See details about a web page's HTML elements as you hover over them.",
+        chrome: "https://chrome.google.com/webstore/detail/showhtml/hpidplbbhlibnefjdbepbinmanddipne",
+        opera: "https://addons.opera.com/en/extensions/details/showhtml/",
+        firefox: "https://addons.mozilla.org/en-US/firefox/addon/showhtml/"
+    }
+];
+
+// OSS slides
+var ossSlides = [
+    {
+        img: "fontSwitcher128.png",
+        title: "fontSwitcher",
+        descr: "Dynamically add Google & Adobe web fonts and apply them on the fly. Create or add font stacks with a single line of JavaScript.",
+        github: "https://github.com/freginold/fontSwitcher",
+    },
+    {
+        img: "thinIt128.png",
+        title: "thinIt",
+        descr: "A (very) basic JavaScript/CSS minifier written in Python.",
+        github: "https://github.com/freginold/thinIt",
+    },
+    {
+        img: "setTimeoutLoop128.png",
+        title: "setTimeoutLoop",
+        descr: "setTimeoutLoop is a JavaScript function designed to allow convenient dynamic calling of a function a designated number of times, with a designated delay.",
+        bitbucket: "https://bitbucket.org/freginold/settimeoutloop",
+    },
+    {
+        img: "trim128.png",
+        title: "trim",
+        descr: "A basic JavaScript trim function to remove lead and trail white space and control characters.",
+        github: "https://github.com/freginold/trim",
+    },
+    {
+        img: "dice128.png",
+        title: "Dice",
+        descr: "Dice slices text nodes up into single-character elements for more detailed styling.",
+        github: "https://github.com/freginold/Dice",
+    }
+];
